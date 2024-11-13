b0a7799af088fe7c2da6cca1d93d31917931af9d
Cătălina Tănase <catalinatanasem@yahoo.com>
2024-11-13 08:30:31 +0200

    B1

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var05/PracticalTest01Var05MainActivity.kt
@@ -0,0 +1,60 @@
+package ro.pub.cs.systems.eim.practicaltest01var05
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PracticalTest01Var05MainActivity : AppCompatActivity() {
+    private lateinit var editTextField: EditText
+    private lateinit var buttonTopLeft: Button
+    private lateinit var buttonTopRight: Button
+    private lateinit var buttonCenter: Button
+    private lateinit var buttonBottomLeft: Button
+    private lateinit var buttonBottomRight: Button
+    private lateinit var buttonTransfer: Button
+
+    private val buttonClickListener = ButtonClickListener()
+    private inner class ButtonClickListener : View.OnClickListener {
+        override fun onClick(view: View) {
+            when (view.id) {
+                R.id.button_top_left -> appendText("Top Left")
+                R.id.button_top_right -> appendText("Top Right")
+                R.id.button_center -> appendText("Center")
+                R.id.button_bottom_left -> appendText("Bottom Left")
+                R.id.button_bottom_right -> appendText("Bottom Right")
+            }
+        }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_practical_test01_var05_main)
+        editTextField = findViewById(R.id.text_field)
+        buttonTopLeft = findViewById(R.id.button_top_left)
+        buttonTopRight = findViewById(R.id.button_top_right)
+        buttonCenter = findViewById(R.id.button_center)
+        buttonBottomLeft = findViewById(R.id.button_bottom_left)
+        buttonBottomRight = findViewById(R.id.button_bottom_right)
+        buttonTransfer = findViewById(R.id.button_transfer)
+
+
+        buttonTopLeft.setOnClickListener(buttonClickListener)
+        buttonTopRight.setOnClickListener(buttonClickListener)
+        buttonCenter.setOnClickListener(buttonClickListener)
+        buttonBottomLeft.setOnClickListener(buttonClickListener)
+        buttonBottomRight.setOnClickListener(buttonClickListener)
+
+    }
+    private fun appendText(text: String) {
+        val currentText = editTextField.text.toString()
+        val newText = if (currentText.isEmpty()) text else "$currentText, $text"
+        editTextField.setText(newText)
+    }
+}
